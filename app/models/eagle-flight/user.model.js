e6aa2453ee6edf58931291ae1a7f9b7669a074a0
PrasmitP <p.pokhrel@eagles.oc.edu>
2025-03-05 18:07:40 -0600

    Added isAdmin to user.model

--- app/models/eagle-flight/user.model.js
+++ app/models/eagle-flight/user.model.js
@@ -16,7 +16,11 @@ module.exports = (sequelize, Sequelize) => {
     email: {
       type: Sequelize.STRING,
       allowNull: false,
-    }
+    },
+    isAdmin: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false,
+    },
   });
 
   return User;
