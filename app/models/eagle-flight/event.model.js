bb9cc5e8be125686a7112693c7185aae6d6a194a
ShadowCore <JohnathonLCortez@yahoo.com>
2025-04-16 14:40:32 -0500

    Slight update to event fields for date specifically.

--- app/models/eagle-flight/event.model.js
+++ app/models/eagle-flight/event.model.js
@@ -13,7 +13,7 @@ module.exports = (sequelize, Sequelize) => {
             type: Sequelize.STRING,
             allowNull: false,
         },
-        dateTime: {
+        date: {
             type: Sequelize.DATE,
             allowNull: true,
         },
