61766cf89adfe7053b732f3cb3c4345b6d684e54
dsantos-emps <david.santos@eagles.oc.edu>
2025-04-21 16:21:58 -0500

    Update server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ require("./app/routes/eagle-flight/student.routes.js")(app);
 require("./app/routes/eagle-flight/task.routes.js")(app);
 require("./app/routes/eagle-flight/auth.routes.js")(app);
 require("./app/routes/eagle-flight/badge.route.js")(app);
-require("./app/routes/eagle-flight/redeemable.route.js")(app);
+// require("./app/routes/eagle-flight/redeemable.route.js")(app);
 require("./app/routes/eagle-flight/event.routes.js")(app);
 
 require("./app/routes/eagle-flight/redeemable.routes.js")(app);
